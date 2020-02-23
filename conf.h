c1ca801b49f7c47ad50a3768b533dd6206b2f384
Kyle Evans <kevans@FreeBSD.org>
2020-02-22 19:39:44 -0600

    conf: add a method for merging another bsm_set into an array
    Signed-off-by: Kyle Evans <kevans@FreeBSD.org>

--- conf.h
+++ conf.h
@@ -35,6 +35,7 @@ extern int 		lineno;
 
 struct bsm_set		*conf_get_bsm_set(char *);
 struct bsm_sequence	*conf_get_parent_sequence(char *);
+void			 conf_merge_bsm_set(struct array *desta, struct bsm_set *src);
 void			 conf_load(char *);
 void			 conf_detail(int, const char *, ...) __attribute__ ((noreturn));
 void			 conf_handle_multiplier(struct bsm_sequence *,
